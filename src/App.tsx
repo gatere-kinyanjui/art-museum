40d2068344bbd39915997cbb3cfa1b2c7d7d8111
GatereKinyanjui <gaterekinyanjui@gmail.com>
2024-01-09 13:21:47 +0300

    completed a-m-p-one

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import "./App.css";
 import GalleryNavigation from "./components/gallery-navigation";
 import havardArt from "./data/havardArt";
-import { IRecord } from "./lib/interfaces";
-
-const fixGalleries: IRecord = havardArt.records;
 
 function App() {
   return (
-    <>
-      <h1>Art Museum</h1>
-      <GalleryNavigation galleries={fixGalleries} />
-    </>
+    <div>
+      <GalleryNavigation galleries={havardArt.records} />
+    </div>
   );
 }
 
--- src/components/gallery-navigation.tsx
+++ src/components/gallery-navigation.tsx
@@ -1,20 +1,30 @@
 import { NavLink } from "react-router-dom";
 import { IRecord } from "../lib/interfaces";
 
-const GalleryNavigation = ({ galleries }: IRecord) => {
+const GalleryNavigation = ({ galleries }: { galleries: IRecord[] }) => {
   console.log(galleries);
 
   return (
-    <NavLink
-      to={"/"}
-      style={({ isActive, isPending, isTransitioning }) => {
-        return {
-          fontWeight: isActive ? "bold" : "",
-          color: isPending ? "red" : "black",
-          viewTransitionName: isTransitioning ? "slide" : "",
-        };
-      }}
-    ></NavLink>
+    <nav>
+      <h1 className="galleries-title">Galleries</h1>
+      <NavLink to={"/"}>Home</NavLink>
+
+      {galleries.map((gallery) => (
+        <NavLink
+          to={`/gallery/${gallery.id}`}
+          key={gallery.id} // Assuming id is unique for each gallery
+          style={({ isActive, isPending, isTransitioning }) => {
+            return {
+              fontWeight: isActive ? "bold" : "",
+              color: isPending ? "red" : "black",
+              viewTransitionName: isTransitioning ? "slide" : "",
+            };
+          }}
+        >
+          {gallery.name}
+        </NavLink>
+      ))}
+    </nav>
   );
 };
 
